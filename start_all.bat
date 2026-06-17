719445654525eb842ab77d870f7ea77298fd8e31
genzabis <syahputranabil521@gmail.com>
2026-06-17 15:54:49 +0700

    fix(start_all): fully sanitize cmd quotation and directory changing syntax

--- start_all.bat
+++ start_all.bat
@@ -8,15 +8,15 @@ REM Set multi-worker supaya request paralel tidak deadlock (PHP 7.4+ Windows)
 set PHP_CLI_SERVER_WORKERS=4
 
 REM Deteksi lokasi PHP
-set PHP_BIN=php
-if exist "C:\xampp\php\php.exe" set PHP_BIN=C:\xampp\php\php.exe
-if exist "D:\xampp\php\php.exe" set PHP_BIN=D:\xampp\php\php.exe
+set "PHP_BIN=php"
+if exist "C:\xampp\php\php.exe" set "PHP_BIN=C:\xampp\php\php.exe"
+if exist "D:\xampp\php\php.exe" set "PHP_BIN=D:\xampp\php\php.exe"
 
-REM Buka 4 window terpisah, masing-masing langsung php -S di working directory yg benar
-start "AppsBank :8000"       /D "%ROOT%apps_bank"       cmd /k %PHP_BIN% -S localhost:8000 -t "%ROOT%apps_bank"
-start "AppsEcommerce :8001"  /D "%ROOT%apps_ecommerce"  cmd /k %PHP_BIN% -S localhost:8001 -t "%ROOT%apps_ecommerce"
-start "AppsPendidikan :8002" /D "%ROOT%apps_pendidikan" cmd /k %PHP_BIN% -S localhost:8002 -t "%ROOT%apps_pendidikan"
-start "AppsTravel :8003"     /D "%ROOT%apps_travel"     cmd /k %PHP_BIN% -S localhost:8003 -t "%ROOT%apps_travel"
+REM Buka 4 window terpisah, pindah direktori, lalu jalankan server PHP
+start "AppsBank :8000"       cmd /k "cd /d "%ROOT%apps_bank" & "%PHP_BIN%" -S localhost:8000 -t "%ROOT%apps_bank""
+start "AppsEcommerce :8001"  cmd /k "cd /d "%ROOT%apps_ecommerce" & "%PHP_BIN%" -S localhost:8001 -t "%ROOT%apps_ecommerce""
+start "AppsPendidikan :8002" cmd /k "cd /d "%ROOT%apps_pendidikan" & "%PHP_BIN%" -S localhost:8002 -t "%ROOT%apps_pendidikan""
+start "AppsTravel :8003"     cmd /k "cd /d "%ROOT%apps_travel" & "%PHP_BIN%" -S localhost:8003 -t "%ROOT%apps_travel""
 
 echo.
 echo Menunggu server siap...
